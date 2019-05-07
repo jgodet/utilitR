e343cf0c2ff252ff104b9dec4d7102d794288e46
jgodet <jgodet@unistra.fr>
2019-05-07 16:42:42 +0200

    commit

--- R/addSmoothSplineLine.r
+++ R/addSmoothSplineLine.r
@@ -12,19 +12,21 @@
 #' @examples
 #' n <- 1e3
 #' dat <- data.frame(
-#'   x = 1:n,
+#'   x = (1:n)+50,
 #'   y = sin(seq(0, 5*pi, length.out = n)) + rnorm(n=n, mean = 0, sd=0.1)
 #' )
 #' plot(dat)
-#' addSmoothSplineLine(dat$y,col='red')
+#' addSmoothSplineLine(y=dat$y,x=dat$x,col='blue')
 #' @export
 
 
 addSmoothSplineLine<- function(y, x = NULL,col="blue",lwd=2,...){
-  yss <- smooth.spline(y)
+
   if(!is.null(x)){
-   lines(x,yss, col=col,lwd=lwd)
+    yss <- smooth.spline(x=x,y=y)
+   lines(yss, col=col,lwd=lwd)
   }else{
+    yss <- smooth.spline(y)
     lines(yss, col=col,lwd=lwd)
   }
   return()
