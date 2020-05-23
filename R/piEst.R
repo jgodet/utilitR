b4d38eb06f0e2fc9142ce9862dd825ecbaa1d819
jgodet <jgodet@unistra.fr>
2020-05-23 17:51:06 +0200

    s

--- R/piEst.R
+++ R/piEst.R
@@ -54,7 +54,7 @@ piEst <- function(y=12, n=20, beta.priors =c(.5,.5),credMass = 0.95, showPlot = 
   res <- data.frame(p_obs = round(p,4), Pi.est = round(E,4), HDI = round(hdi,4),CI =round(unname(ci),4), Binom = round(bt$conf.int[1:2],4))
   res[2,1:2] <- ""
   if(lateX){
-    kableExtra::kable(t(res), format = "latex")
+    kable(t(res), format = "latex")
   }else{
     stargazer(t(res), type="text",single.row=FALSE,summary=FALSE)
   }
