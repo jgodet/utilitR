4fd0a3f88ddd5376dcfba0c15f092a559b354bd7
jug <jgodet@unistra.fr>
2024-05-14 12:36:11 +0200

    updt

--- R/newRmdRepport.R
+++ R/newRmdRepport.R
@@ -20,7 +20,7 @@
 
 
 newRmdRepport<- function(nom = "NOM", prenom = "PRENOM", path = NULL, filename = NULL){
-  st1 <-"---\noutput:\n  pdf_document:\n    keep_tex: true\n\nurlcolor: blue\nheader-includes:\n   -\\definecolor{oldlace}{rgb}{1.00, 0.976,0.93}\n   -\\usepackage[francais]{babel}\n   -\\usepackage{lmodern}\n---\n\n```{r setup, include=FALSE}\nknitr::opts_chunk$set(echo = FALSE, message = FALSE, warning=FALSE)\n```\n"
+  st1 <-"---\noutput:\n  pdf_document:\n    keep_tex: true\n\nurlcolor: blue\nheader-includes:\n   - \\definecolor{oldlace}{rgb}{1.00, 0.976,0.93}\n   - \\usepackage[francais]{babel}\n   - \\usepackage{lmodern}\n---\n\n```{r setup, include=FALSE}\nknitr::opts_chunk$set(echo = FALSE, message = FALSE, warning=FALSE)\n```\n"
   #st2 <-paste("\n\\definecolor{oldlace}{rgb}{1.00, 0.976,0.93}\n\\newcommand{\\nom}{",nom,"} % le nom du clinicien (titre du rapport et bas de page)\n\\newcommand{\\prenom}{",prenom,"}% le prenom du clinicien (titre du rapport et bas de page)\n\n\n",sep='')
   #st3 <-"\\voffset -0.2 in \n\\textheight 20cm \n\\headheight 0.6cm \n\\headsep 1 cm	\n\n\n\n\n\\begin{document}\n"
   if(length(grep("macOS",sessionInfo()$running))>=1){
@@ -37,7 +37,7 @@ newRmdRepport<- function(nom = "NOM", prenom = "PRENOM", path = NULL, filename =
       }
   st5 <-"\\vfill\n\\vfill\n\\vfill\n\\vfill\n\\vfill\n\\vfill\n\\vfill\n\\vfill\n\\vfill\n"
   st6 <-"\\begin{minipage}{0.75\\textwidth}\n\\begin{flushright}\n
-\\emph{Julien GODET}\\\\ \n\\emph{MCU-PH}\\\\ \n
+\\emph{Julien GODET}\\\\ \n\\emph{PU-PH}\\\\ \n
 \\emph{P\\^ole de Sant\\'e Publique}\\\\ \n\\emph{\\href{mailto:julien.godet@chru-strasbourg.fr}{julien.godet@chru-strasbourg.fr}}\\\\\ \n\\emph{\\href{mailto:jgodet@unistra.fr}{julien.godet@unistra.fr}} \n~\\\\ \n\\emph{\\today}\\\\ \n"
   st7 <-"\\end{flushright}\n\\end{minipage}\n\\vfill\n\\vfill\n\\end{center}\n\\end{titlepage}\n\\newpage\n\\tableofcontents\n\\newpage\n\n\n"
   st8 <-"# Objectifs et critères de jugement\n"
